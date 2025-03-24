22edd118373320204f3d8f7b9f2e327e1854b34d
Alex Schinnerl <alex.itsme@gmx.at>
2025-03-24 12:59:24 +0100

    docs folder

--- convert_notebook.py
+++ convert_notebook.py
@@ -2,4 +2,4 @@ import os
 
 os.system("jupyter nbconvert --to html damageCalculation.ipynb --output-dir='./output' --output damageCalculation_inclCode")
 os.system("jupyter nbconvert --to html --no-input damageCalculation.ipynb --output-dir='./output' --output damageCalculation_exclCode")
-os.system("jupyter nbconvert --to html --no-input damageCalculation.ipynb --output-dir='./output' --output index")
+os.system("jupyter nbconvert --to html --no-input damageCalculation.ipynb --output-dir='./docs' --output index")
